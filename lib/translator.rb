19997297dc7217c4f49f716ed3dac7fb443e4d5d
Maximo Bautista <max.bautista97@gmail.com>
2019-01-18 17:13:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(file_path, emoji)
       return library[key][emoji]
     end
   end
-  return "Sorry emoticon not found."
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoji)
@@ -32,5 +32,5 @@ def get_english_meaning(file_path, emoji)
       return library[key][emoji]
     end
   end
-  return "Sorry emoticon not found."
+  return "Sorry, that emoticon was not found"
 end
