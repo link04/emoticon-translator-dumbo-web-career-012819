4b13164ae9a8d0b9cb0225c6324f5112ed121629
Maximo Bautista <max.bautista97@gmail.com>
2019-01-18 17:12:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path, emoji)
   library = load_library(file_path)
   library.each do |key, value|
     if library[key][emoji]
-      
+      return library[key][emoji]
     end
   end
   
